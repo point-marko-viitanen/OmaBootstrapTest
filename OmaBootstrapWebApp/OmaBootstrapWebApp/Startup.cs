4ceabf99eaa1965e94a7d61ee4b25074cc71f542
Marko Viitanen <marko.viitanen@stud.pointcollege.fi>
2016-10-30 17:09:20 +0200

    Perustettu mvc app

---
+++ OmaBootstrapWebApp/OmaBootstrapWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OmaBootstrapWebApp.Startup))]
+namespace OmaBootstrapWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
